b25e5471f731d35d90bf80f535d376dd83f273b7
James Devine <t-jamdev@microsoft.com>
2022-03-14 11:55:55 +0000

    upgrade release script

--- release.sh
+++ release.sh
@@ -1,3 +1,10 @@
-python3 -m pip install --upgrade build 
-python3 -m build 
-twine upload -r pypi dist/*
+if [ "$1" = "part" ] || [ "$1" = "minor" ] || [ "$1" = "major" ]
+then
+    python3 -m pip install --upgrade bumpversion
+    python3 -m bumpversion --config-file .bumpversion.cfg $1 
+    python3 -m pip install --upgrade build 
+    python3 -m build 
+    twine upload -r pypi dist/*
+else
+    echo "Please indicate the level to bump: part, minor, major"
+fi
